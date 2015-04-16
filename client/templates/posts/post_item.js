6d0f6fa085d207c20155bd1784441acc4f6d86bf
Greg Jones <gjjones@ebsco.com>
2015-04-16 07:18:35 -0400

    Adds naive voting feature.
    Displays vote count.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,5 +6,12 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  },
+  }
+});
+
+Template.postItem.events({
+  'click .upvote': function (e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
 });
